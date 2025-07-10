56de176dfe1048a978191548cd54b3538ba06606
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-07-11 01:57:03 +0200

    Add column is_owner_mogul

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from pous!")
+import pandas as pd
+
+mogul_owned_clubs = [
+    "Arsenal",
+    "Aston Villa",
+    "Blackburn",
+    "Chelsea",
+    "Everton",
+    "Fulham",
+    "Liverpool",
+    "Man City",
+    "Man Utd",
+    "Newcastle",
+    "Tottenham",
+    "West Ham",
+]
 
 
-if __name__ == "__main__":
-    main()
+df = pd.read_csv("transfers.csv")
+
+df.columns = df.columns.str.strip()
+
+df["is_owner_mogul"] = df["to_club_name"].isin(mogul_owned_clubs).astype(int)
+
+df.to_csv("transfers.csv", index=False)
