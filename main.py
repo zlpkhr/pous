7aad58e61e04e27627445e0c180f343e8e3f2243
Azamat Zulpykhar <zlpkhr@icloud.com>
2025-07-11 23:33:19 +0200

    Add foot

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-stages = [
-    "Premier League",
-    "LaLiga",
-    "Serie A",
-    "Bundesliga",
-    "Ligue 1",
-    "Championship",
-    "League One",
-    "League Two",
-    "Major League Soccer",
-    "Eredivisie",
-    "Liga Portugal",
-    "Premiership",
-    "Brasileirão",
-    "Super Lig",
-    "Premier League (RU)",
-    "Jupiler Pro League",
-    "2. Bundesliga",
-]
+import pandas as pd 
+
+tdf = pd.read_csv('transfers.csv')
+pdf = pd.read_csv('players.csv')
+
+# Map the 'foot' column from pdf to tdf using 'player_id'
+foot_map = pdf.set_index('player_id')['foot']
+
+# Create a new Series in tdf for 'foot'
+tdf['foot'] = tdf['player_id'].map(foot_map)
+
+# Define functions to set plays_left_foot and plays_right_foot
+def left_foot_val(foot):
+    if pd.isna(foot):
+        return ''
+    return 1 if foot in ['left', 'both'] else 0
+
+def right_foot_val(foot):
+    if pd.isna(foot):
+        return ''
+    return 1 if foot in ['right', 'both'] else 0
+
+tdf['plays_left_foot'] = tdf['foot'].apply(left_foot_val)
+tdf['plays_right_foot'] = tdf['foot'].apply(right_foot_val)
+
+# Drop the helper 'foot' column
+tdf.drop(columns=['foot'], inplace=True)
+
+# Save the updated DataFrame back to transfers.csv
+tdf.to_csv('transfers.csv', index=False)
